98d4011eb28e3629bd4ed8c6b291446dd61de72e
bryanbalderas <bryanpbalderas@gmail.com>
2021-03-14 22:29:49 -0600

    tienda

--- codigo/main.js
+++ codigo/main.js
@@ -30,7 +30,8 @@ let products=[
 
 for (let i=0; i< carts.length;i++){
     carts[i].addEventListener('click',()=>{
-        cartNumbers();
+        cartNumbers(products[i]);
+        totalCost(products[i]);
     })
 }
 
@@ -43,7 +44,7 @@ function onLoadCartNumbers(){
 }
 
 
-function cartNumbers(){
+function cartNumbers(product){
     let productNumbers=localStorage.getItem('cartNumbers');
     productNumbers=parseInt(productNumbers);
     
@@ -54,8 +55,91 @@ function cartNumbers(){
         localStorage.setItem('cartNumbers',1);
         document.querySelector('.ContadorCarrito').textContent=1;
     }
+
+    setItems(product);
 }
 
-///https://www.youtube.com/watch?v=PoTGs38DR9E
+
+function setItems(product){
+    let cartItems=localStorage.getItem('productsInCart');
+    cartItems=JSON.parse(cartItems);
+
+    if(cartItems!=null){
+
+        if(cartItems[product.tag]==undefined){
+            cartItems={
+                ...cartItems,
+                [product.tag]:product
+            }
+        }
+        cartItems[product.tag].inCart+=1;
+    }else{
+        product.inCart=1;
+        cartItems={
+            [product.tag]:product
+        }
+    }
+    localStorage.setItem("productsInCart",JSON.stringify(cartItems) );
+
+}
+
+function totalCost(product){
+    let cartCost=localStorage.getItem('totalCost');
+
+    if(cartCost!=null){
+        cartCost=parseInt(cartCost);
+        localStorage.setItem("totalCost",cartCost+product.price)
+    }else{
+        localStorage.setItem("totalCost",product.price)
+    }
+
+}
+
+
+function displayCart(){
+    let cartItems=localStorage.getItem("productsInCart");
+    cartItems=JSON.parse(cartItems);
+
+    let productContainer=document.querySelector(".products");
+    let cartCost=localStorage.getItem('totalCost');
+
+
+    if(cartItems && productContainer){
+        productContainer.innerHTML='';
+        Object.values(cartItems).map(item => {
+            productContainer.innerHTML += `
+            <div class="product">
+                <ion-icon name="close-circle-outline"></ion-icon>
+                <span Style="Color:white">${item.name} </span>
+                <img src="Imagenes/${item.tag}.jpeg" width="250px" height="250px">
+            </div>
+            <div class="price"> precio:${item.price}</div>
+            <div class="quantity">cantidad:
+                <ion-icon class="decrease" name="arrow-dropleft-circle"></ion-icon>
+                <span>${item.inCart}</span>
+                <ion-icon class="increase" name="arrow-dropright-circle"></ion-icon>
+            </div>
+            <div class="total">
+                $${item.inCart*item.price}.00
+            </div>
+            `
+        });
+
+        productContainer.innerHTML+=`
+            <div class="basketTotalContainer">
+                <h4 class="basketTotalTitle">
+                    Canasta Total
+                </h4>
+                <h4 class="basketTotal">
+                    $${cartCost}.00
+                </h4>
+            </div>
+        `
+
+    }
+}
+
+///https://www.youtube.com/watch?v=QNXQfdgIXLw
 
 onLoadCartNumbers();
+displayCart();
